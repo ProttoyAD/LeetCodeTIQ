58b21dec7e4b95e29d64c5008f2a22b566be7aff
ProttoyAD <prottoyadhusher@gmail.com>
2022-10-29 19:38:38 +0600

    Leetcode

--- TIQ/src/DayOne/Dolon.java
+++ TIQ/src/DayOne/Dolon.java
@@ -3,5 +3,6 @@ package DayOne;
 public class Dolon {
     public static void main(String[] args) {
         System.out.println("HI i am dolon");
+        System.out.println("I am married");
     }
 }
